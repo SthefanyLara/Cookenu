542ce43db626c62580f362886901fd5d391d77d3
Sthefany Lara <88106798+SthefanyLara@users.noreply.github.com>
2021-11-09 16:14:24 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { ThemeProvider } from '@material-ui/styles';
+import React, { useState } from 'react'
+import Router from './routes/Router'
+import theme from './constants/theme'
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+
+const App = () => {
+  const token = localStorage.getItem("token")
+  const [rightButtonText, setRightButtonText] = useState(token ? "Logout" : "Login")
+
+  return (
+    
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header rightButtonText={rightButtonText} setRightButtonText={setRightButtonText}/>
+          <Router setRightButtonText={setRightButtonText}/>
+        </BrowserRouter>
+      </ThemeProvider>
+    
+  );
+}
+
+export default App;
